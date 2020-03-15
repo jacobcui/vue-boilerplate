b09b4410013b1383978179fbe57ec010587022a6
Jacob CUI <jacobcui123@gmail.com>
2020-03-16 00:10:33 +1100

    adding routers

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>{{ msg }}</h1>
+    <a href="/">Home</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'Welcome to About page.'
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>{{ msg }}</h1>
+    <a href="/about">About</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Home page.'
+    }
+  }
+}
+</script>
